bc7472341886e4bd350b9a0bfa02c09491ed7907
David Chanter <dchanter@gmail.com>
2018-05-24 08:39:48 +1000

    Add files via upload

---
+++ TAB-ColdGun-v3.ino
@@ -0,0 +1,77 @@
+
+
+
+
+#include "FastLED.h"
+#define NUM_LEDS 14 
+CRGB leds[NUM_LEDS];
+#define PIN 17
+#define FLED1 16
+#define FLED2 21
+#define FLED3 5
+
+void setup()
+{
+  FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+}
+
+void loop() {
+  colorWipe(0x00,0x00,0xff, 75);
+  delay(250);
+  colorWipe(0x00,0x00,0x00, 50);
+
+for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(FLED1, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(20);
+  }
+
+  // fade out from max to min in increments of 5 points:
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(FLED1, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(20);
+  }
+  
+}
+
+void colorWipe(byte red, byte green, byte blue, int SpeedDelay) {
+  for(uint16_t i=0; i<NUM_LEDS; i++) {
+      setPixel(i, red, green, blue);
+      showStrip();
+      delay(SpeedDelay);
+  }
+}
+
+void showStrip() {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.show();
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   FastLED.show();
+ #endif
+}
+
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H 
+   // FastLED
+   leds[Pixel].r = red;
+   leds[Pixel].g = green;
+   leds[Pixel].b = blue;
+ #endif
+}
+
+void setAll(byte red, byte green, byte blue) {
+  for(int i = 0; i < NUM_LEDS; i++ ) {
+    setPixel(i, red, green, blue); 
+  }
+  showStrip();
+}
